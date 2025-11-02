cda5baac07e87ef718db82545687f246b9ccf147
iamdiegoriveros <iamdiegoriveros@gmail.com>
2025-11-02 16:55:55 -0300

    creation of the entity airline

---
+++ src/main/java/com/flight_booking/airline/entity/Airline.java
@@ -0,0 +1,39 @@
+package com.flight_booking.airline.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "airlines")
+public class Airline {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
